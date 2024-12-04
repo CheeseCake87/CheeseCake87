af810d6f080e2f59bb0d57db0dd62e103d3df3cb
David Carmichael <carmichaelits@gmail.com>
2024-12-04 22:12:03 +0000

    add filter example

--- jinja-issues/1934.py
+++ jinja-issues/1934.py
@@ -3,11 +3,12 @@ from jinja2 import Environment
 import json
 
 env = Environment()
+env.filters["rawjson"] = json.dumps
 
-t = env.from_string("{{ x|tojson }}")
+t = env.from_string("{{ x|rawjson }} <p></p>")
 
 print(t.render(x={"html": "<p class=\"this\"> foo and 'bar' </p>"}))
 # print(t.render(x="<bar>"))
 
-value = "'"
-print(json.dumps(value))
+# value = "'"
+# print(json.dumps(value))
